377759a95eddb13e66a891683d6d6727416c6364
Guillaume <guillaume.peyrelongue@devcoop.fr>
2023-07-19 22:11:34 +0200

    Exercice 2 : récupérer la liste des écritures pour **UN** compte

--- src/Ecritures/gestionEcritures.php
+++ src/Ecritures/gestionEcritures.php
@@ -9,5 +9,32 @@ use Slim\App;
 
 
 return function ($app,$conn) {
+    // --Exercice 2 : récupérer la liste des écritures pour **UN** compte -- //
+    $app->get('/comptes/{uuid}/ecritures', function (Request $request, Response $response, array $args) use ($conn) {
+        $uuid = $args['uuid'];
+        $sql = "SELECT * FROM ecritures WHERE compte_uuid = :uuid";
 
+        try {
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':uuid', $uuid);
+            $stmt->execute();
+            $ecritures = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $db = null;
+
+            $response->getBody()->write(json_encode($ecritures));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(200);
+        } catch (PDOException $e) {
+            $error = array(
+                "message" => $e->getMessage()
+            );
+
+            $response->getBody()->write(json_encode($error));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(500);
+        }
+    });
+    // -- 2 -- //
 };
